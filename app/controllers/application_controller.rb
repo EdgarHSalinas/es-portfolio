1dc42afaf4ea40b3f9ca85e0773c2f0cabc3f6f6
Edgar <edgar.h.salinas@gmail.com>
2018-07-20 13:18:38 -0700

    Refactor of Concerns of guest_user and renamed the rest of concerns to include the word Concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  include DeviseWhitelist
-  include SetSource
-
-  # Override current_user method (overloading in Java)
-  # Null Object Pattern
-  def current_user
-    super || OpenStruct.new(name: 'Guest User',
-                            first_name: 'Guest',
-                            last_name: 'User',
-                            email: 'guest@example.com')
-  end
+  include DeviseWhitelistConcern
+  include SetSourceConcern
+  include CurrentUserConcern
 end
